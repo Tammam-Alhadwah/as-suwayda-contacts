353d794b5ce38d930258e831e94478aa13b51632
Tammam Alhadwah <t.alhadwah@gmail.com>
2026-02-25 00:08:04 +0300

    V2.0

--- sw.js
+++ sw.js
@@ -1,54 +1,41 @@
-const CACHE_NAME = 'suwayda-emergency-v2';
-const ASSETS_TO_CACHE = [
+const CACHE_VERSION = 'v3';
+const CACHE_NAME = `suwayda-emergency-${CACHE_VERSION}`;
+
+const STATIC_ASSETS = [
     './',
     './index.html',
     './manifest.json',
+    './contacts.json',
     './icon-192.png',
     './icon-512.png',
-    // We cache the CDNs so they work offline too
+    'https://unpkg.com/leaflet/dist/leaflet.css',
+    'https://unpkg.com/leaflet/dist/leaflet.js',
     'https://fonts.googleapis.com/css2?family=Cairo:wght@400;600;700&display=swap',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
-self.addEventListener('activate', event => {
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys.filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-            );
-        })
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(STATIC_ASSETS))
     );
 });
 
-// Install Event: Cache files
-self.addEventListener('install', (event) => {
+self.addEventListener('activate', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                return cache.addAll(ASSETS_TO_CACHE);
-            })
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
     );
 });
 
 self.addEventListener('fetch', event => {
-    if (event.request.url.includes('tile.openstreetmap')) {
-        event.respondWith(
-            caches.open(CACHE_NAME).then(cache => {
-                return cache.match(event.request).then(response => {
-                    return response || fetch(event.request).then(networkResponse => {
-                        cache.put(event.request, networkResponse.clone());
-                        return networkResponse;
-                    });
-                });
-            })
-        );
-        return;
-    }
-
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
     );
 });
