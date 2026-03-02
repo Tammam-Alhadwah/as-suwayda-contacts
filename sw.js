8dee8ea07d837781ccf2bc1844ce8223cef2f41c
Tammam Alhadwah <t.alhadwah@gmail.com>
2026-03-02 09:26:36 +0300

    V17

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v16'; // Bumped version to trigger update
+const CACHE_VERSION = 'v17'; // Bumped version to trigger update
 const CACHE_NAME = `suwayda-emergency-${CACHE_VERSION}`;
 
 const STATIC_ASSETS = [
